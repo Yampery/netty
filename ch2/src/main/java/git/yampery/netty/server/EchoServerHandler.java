5c34b4c3f9ebc9820bda6f8aeb2176a685bed181
yampery <yampery@163.com>
2018-02-26 23:48:48 +0800

    添加==> Echo服务处理器逻辑

--- ch2/src/main/java/git/yampery/netty/server/EchoServerHandler.java
+++ ch2/src/main/java/git/yampery/netty/server/EchoServerHandler.java
@@ -1,10 +1,43 @@
 package git.yampery.netty.server;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
 /**
  * @decription: EchoServerHandler
  * <p>服务端处理器</p>
  * @date 18/2/26 23:33
  * @author yampery
  */
-public class EchoServerHandler {
+@Sharable   // 标示一个ChannelHandler可以被多个Channel安全地共享
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        // 将消息打印到控制台
+        System.out.println("Server Received ==> " + in.toString(CharsetUtil.UTF_8));
+        // 将接收到的消息写给发送者，并不冲刷数据
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // 冲刷所有待审核消息到远程节点，并关闭该通道
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // 打印异常堆栈跟踪
+        cause.printStackTrace();
+        // 关闭通道
+        ctx.close();
+    }
 }
