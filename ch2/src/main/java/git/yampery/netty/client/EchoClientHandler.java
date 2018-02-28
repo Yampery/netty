38c2582188b9fd3cf25efabc680c295b600bf233
yampery <yampery@163.com>
2018-02-28 22:51:19 +0800

    更改==> 添加EchoClientHandler代码逻辑

--- ch2/src/main/java/git/yampery/netty/client/EchoClientHandler.java
+++ ch2/src/main/java/git/yampery/netty/client/EchoClientHandler.java
@@ -1,10 +1,48 @@
 package git.yampery.netty.client;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
 /**
  * @decription: EchoClientHandler
  * <p>客户端处理器</p>
  * @date 18/2/26 23:34
  * @author yampery
  */
-public class EchoClientHandler {
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    /**
+     * 服务器的链接被建立后调用
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // 当被通知Channel是活跃的时候发送一条消息
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 数据被服务器接收调用
+     * @param ctx
+     * @param in
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        // 记录已接收消息的转储
+        System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // 打印异常
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
